d7763f211e285b1bea8c469fa286bdff839005e2
Olivian123 <ionmargesvu@gmail.com>
2023-02-08 17:33:49 +0200

    corectare comentariu

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -2,7 +2,7 @@
 public interface Command {
 
     /* the classes that implement this interface, to keep the signature of "doCommand", */
-    /* will store as attributes the parameters that they need to do their commands */
+    /* will store as attributes the parameters that they need to use in their commands */
 
     /* every class that implements a command will overwrite this method*/
     void doCommand ();
